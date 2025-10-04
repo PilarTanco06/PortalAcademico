7d16203a1fab6fb0dab453b099276a2895322fb2
Pilar <pilartq@gmail.com>
2025-10-04 14:05:46 -0500

    Creación de modelos y configuración de la base de datos

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PortalAcademico.Models;
+
+namespace PortalAcademico.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Código único
+            builder.Entity<Curso>()
+                .HasIndex(c => c.Codigo)
+                .IsUnique();
+
+            // Validación: Usuario no puede matricularse dos veces en el mismo curso
+            builder.Entity<Matricula>()
+                .HasIndex(m => new { m.CursoId, m.UsuarioId })
+                .IsUnique();
+
+            // Validación de HorarioInicio < HorarioFin se hará en aplicación
+        }
+    }
+}
